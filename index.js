6a3f0f89a6e56b65aeca847c7b8cf8d515cd474c
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-12-07 15:48:23 +0530

    Html file added

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+server.listen(3000, () => {
+  console.log("listening on port 3000...");
+});
