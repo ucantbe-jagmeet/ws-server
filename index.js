e2466a89e14570f18cf55e64032c962363d6ed87
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-12-07 16:42:30 +0530

    emitting connection through frontend

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
+    io.emit("chat message", msg);
   });
 });
+
 server.listen(3000, () => {
   console.log("listening on *:3000");
 });
