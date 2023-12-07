dd25ea524f1a0ec3c15e080c0adfafe64de1dc3c
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-12-07 16:20:39 +0530

    sending and receiving messages

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
 });
-
 server.listen(3000, () => {
   console.log("listening on *:3000");
 });
